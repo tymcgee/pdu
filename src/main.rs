c4f9bda55b4968247e0be92b72defa73ab8e88ed
tynan <tynanmcgeek@gmail.comm>
2023-08-24 21:01:36 -0700

    Split main into a couple of functions

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ struct PathData {
 }
 
 impl PathData {
-    fn get_human_readable_size(self) -> String {
+    fn get_human_readable_size(&self) -> String {
         let mut out = self.size as f64;
         let mut suffix = "YiB";
         for unit in vec!["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"] {
@@ -41,18 +41,19 @@ impl PathData {
 
 fn main() -> Result<(), std::io::Error> {
     let cwd = current_dir()?;
+    let mut data = get_data_from_directory(cwd)?;
+    print_data(&mut data);
+    Ok(())
+}
 
+fn get_data_from_directory(dir: PathBuf) -> Result<Vec<PathData>, std::io::Error> {
     let mut data: Vec<PathData> = vec![];
-    let mut total: u64 = 0;
-    let mut grid = Grid::new(GridOptions {
-        filling: Filling::Spaces(1),
-        direction: Direction::LeftToRight,
-    });
+    let mut total_size: u64 = 0;
 
-    for file in cwd.read_dir()?.filter_map(|f| f.ok()) {
+    for file in dir.read_dir()?.filter_map(|f| f.ok()) {
         if file.metadata()?.is_dir() {
             let size = get_size_of_directory(file.path());
-            total += size;
+            total_size += size;
             data.push(PathData {
                 size,
                 name: file.file_name(),
@@ -60,7 +61,7 @@ fn main() -> Result<(), std::io::Error> {
             })
         } else if file.metadata()?.is_file() {
             let size = file.metadata()?.len();
-            total += size;
+            total_size += size;
             data.push(PathData {
                 size,
                 name: file.file_name(),
@@ -70,12 +71,22 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     data.push(PathData {
-        size: total,
+        size: total_size,
         name: OsString::from("Total"),
         icon: "".to_string(),
     });
 
+    return Ok(data);
+}
+
+fn print_data(data: &mut Vec<PathData>) {
+    let mut grid = Grid::new(GridOptions {
+        filling: Filling::Spaces(1),
+        direction: Direction::LeftToRight,
+    });
+
     data.sort_by_key(|k| k.size);
+
     for d in data {
         grid.add(Cell::from(d.icon.clone()));
         grid.add(Cell::from(d.name.to_str().unwrap_or("???")));
@@ -83,8 +94,6 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     println!("{}", grid.fit_into_columns(3));
-
-    Ok(())
 }
 
 fn get_size_of_directory(root: PathBuf) -> u64 {
